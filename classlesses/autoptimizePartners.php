7355d710b820023a20ca8f11cf8f67eed2f952c4
zytzagoo <zytzagoo@gmail.com>
2016-07-28 13:16:25 +0200

    upstream's autoptimizePartners.php @1a1abd2 [ci skip]

--- classlesses/autoptimizePartners.php
+++ classlesses/autoptimizePartners.php
@@ -4,7 +4,6 @@ Classlessly add a "more tools" tab to promote (future) AO addons and/ or affilia
 */
 
 add_action('admin_init', 'ao_partner_tabs_preinit');
-
 function ao_partner_tabs_preinit() {
     if (apply_filters('autoptimize_filter_show_partner_tabs',true)) {
         add_filter('autoptimize_filter_settingsscreen_tabs','ao_add_partner_tabs');
@@ -12,14 +11,16 @@ function ao_partner_tabs_preinit() {
 }
 
 function ao_add_partner_tabs($in) {
-	$in=array_merge($in,array('ao_partners' => 'Optimize More!'));
+	$in=array_merge($in,array('ao_partners' => __('Optimize More!','autoptimize')));
 	return $in;
 }
 
 add_action('admin_menu','ao_partners_init');
 function ao_partners_init() {
-	$hook=add_submenu_page(NULL,'AO partner','AO partner','manage_options','ao_partners','ao_partners');
-	// register_settings here as well if needed
+    if (apply_filters('autoptimize_filter_show_partner_tabs',true)) {
+		$hook=add_submenu_page(NULL,'AO partner','AO partner','manage_options','ao_partners','ao_partners');
+		// register_settings here as well if needed
+	}
 }
 
 function ao_partners() {
@@ -28,7 +29,7 @@ function ao_partners() {
 	.itemDetail {
 		background: #fff;
 		width: 250px;
-		height: 270px;
+		min-height: 270px;
 		border: 1px solid #ccc;
 		float: left;
 		padding: 15px;
@@ -37,14 +38,26 @@ function ao_partners() {
 	}
 	.itemTitle {
 		margin-top:0px;
+		margin-bottom:10px;
 	}
 	.itemImage {
+		text-align: center;
+	}
+	.itemImage img {
 		max-width: 95%;
+		max-height: 150px;
+	}
+	.itemDescription {
+		margin-bottom:30px;
+	}
+	.itemButtonRow {
+		position: absolute;
+		bottom: 10px;
+		right: 10px;
+		width:100%;
 	}
 	.itemButton {
-		position:absolute;
-		bottom: 20px;
-		right: 20px;
+		float:right;
 	}
 	.itemButton a {
 		text-decoration: none;
@@ -76,7 +89,7 @@ function getAOPartnerFeed() {
 		$maxitems = 0;
 
 		if ( ! is_wp_error( $rss ) ) {
-			$maxitems = $rss->get_item_quantity( 10 );
+			$maxitems = $rss->get_item_quantity( 20 );
 			$rss_items = $rss->get_items( 0, $maxitems );
 		} ?>
 		<ul>
@@ -84,17 +97,18 @@ function getAOPartnerFeed() {
 			if ( $maxitems == 0 ) {
 				echo $noFeedText;
 			} else {
-				foreach ( $rss_items as $item ) : ?>
+				foreach ( $rss_items as $item ) :
+					$itemURL = esc_url( $item->get_permalink() ); ?>
 					<li class="itemDetail">
-						<h3 class="itemTitle"><a href="<?php echo esc_url( $item->get_permalink() ); ?>"><?php echo esc_html( $item->get_title() ); ?></a></h3>
+						<h3 class="itemTitle"><a href="<?php echo $itemURL; ?>" target="_blank"><?php echo esc_html( $item->get_title() ); ?></a></h3>
 						<?php
 						if (($enclosure = $item->get_enclosure()) && (strpos($enclosure->get_type(),"image")!==false) ) {
-							$img_url=esc_url($enclosure->get_link());
-							echo "<img class=\"itemImage\" src=\"".$img_url."\"/>";
+							$itemImgURL=esc_url($enclosure->get_link());
+							echo "<div class=\"itemImage\"><a href=\"".$itemURL."\" target=\"_blank\"><img src=\"".$itemImgURL."\"/></a></div>";
 						}
 						?>
 						<div class="itemDescription"><?php echo wp_kses_post($item -> get_description() ); ?></div>
-						<div class="itemButton button-secondary"><a href="<?php echo esc_url( $item->get_permalink() ); ?>">More info</a></div>
+						<div class="itemButtonRow"><div class="itemButton button-secondary"><a href="<?php echo $itemURL; ?>" target="_blank">More info</a></div></div>
 					</li>
 				<?php endforeach; ?>
 			<?php } ?>
