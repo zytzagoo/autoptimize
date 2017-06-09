7bc6e2ff5c7530208ac714455355a8b2ba3bcf75
zytzagoo <zytzagoo@gmail.com>
2017-06-09 10:10:27 +0200

    define AUTOPTIMIZE_HASH same as upstream

--- autoptimize.php
+++ autoptimize.php
@@ -342,7 +342,7 @@ function autoptimize_end_buffering($content) {
     }
 
     if ( ! defined( 'AUTOPTIMIZE_HASH' ) ) {
-        define( 'AUTOPTIMIZE_HASH', wp_hash( AUTOPTIMIZE_CACHE_URL . microtime(), 'nonce' ) );
+        define( 'AUTOPTIMIZE_HASH', wp_hash( AUTOPTIMIZE_CACHE_URL . microtime() ) );
     }
 
     // Config element
