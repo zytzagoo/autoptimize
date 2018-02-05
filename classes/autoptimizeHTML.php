44bab20bb451a4c9192492b91e5890451f4e850d
zyt <zytzagoo@gmail.com>
2018-02-05 23:16:13 +0100

    match autoptimizeHTML.php with upstream at d72b5a8 + fix some phpcs issues

--- classes/autoptimizeHTML.php
+++ classes/autoptimizeHTML.php
@@ -1,73 +1,114 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
 class autoptimizeHTML extends autoptimizeBase
 {
     private $keepcomments = false;
+    private $exclude      = array(
+        '<!-- ngg_resource_manager_marker -->',
+        '<!--noindex-->',
+        '<!--/noindex-->',
+    );
 
-    //Does nothing
-    public function read($options)
+    public function read( $options )
     {
-        $noptimizeHTML = apply_filters( 'autoptimize_filter_html_noptimize', false, $this->content );
-        if ( $noptimizeHTML ) {
-            return false;
-        }
-
-        //Remove the HTML comments?
+        // Remove the HTML comments?
         $this->keepcomments = (bool) $options['keepcomments'];
 
-        // filter to force xhtml
-        $this->forcexhtml = apply_filters( 'autoptimize_filter_html_forcexhtml', 'false' );
+        // Filter to force xhtml.
+        $this->forcexhtml = (bool) apply_filters( 'autoptimize_filter_html_forcexhtml', false );
 
-        //Nothing to read for HTML
+        // Filterable strings to be excluded from HTML minification.
+        $exclude = apply_filters( 'autoptimize_filter_html_exclude', '' );
+        if ( '' !== $exclude ) {
+            $exclude_arr   = array_filter( array_map( 'trim', explode( ',', $exclude ) ) );
+            $this->exclude = array_merge( $exclude_arr, $this->exclude );
+        }
+
+        // Nothing else for HTML!
         return true;
     }
 
-    //Joins and optimizes CSS
+    /**
+     * Minifies HTML.
+     *
+     * @return bool
+     */
     public function minify()
     {
-        if ( class_exists( 'Minify_HTML' ) ) {
-            // noptimize me
-            $this->content = $this->hide_noptimize($this->content);
+        $noptimize = apply_filters( 'autoptimize_filter_html_noptimize', false, $this->content );
+        if ( $noptimize ) {
+            return false;
+        }
 
-            // Minify html
+        if ( class_exists( 'Minify_HTML' ) ) {
+            // Wrap the to-be-excluded strings in noptimize tags.
+            foreach ( $this->exclude as $str ) {
+                if ( false !== strpos( $this->content, $str ) ) {
+                    $replacement   = '<!--noptimize-->' . $str . '<!--/noptimize-->';
+                    $this->content = str_replace( $str, $replacement, $this->content );
+                }
+            }
+
+            // Noptimize.
+            $this->content = $this->hide_noptimize( $this->content );
+
+            // Preparing options for Minify_HTML.
             $options = array( 'keepComments' => $this->keepcomments );
             if ( $this->forcexhtml ) {
                 $options['xhtml'] = true;
             }
 
-            if ( is_callable( array( 'Minify_HTML', 'minify' ) ) ) {
-                $tmp_content = Minify_HTML::minify($this->content, $options);
+            if ( @is_callable( array( 'Minify_HTML', 'minify' ) ) ) {
+                $tmp_content = Minify_HTML::minify( $this->content, $options );
                 if ( ! empty( $tmp_content ) ) {
                     $this->content = $tmp_content;
                     unset( $tmp_content );
                 }
             }
 
-            // revslider data attribs somehow suffer from HTML optimization, this fixes that
+            // Restore noptimize.
+            $this->content = $this->restore_noptimize( $this->content );
+
+            // Remove the noptimize-wrapper from around the excluded strings.
+            foreach ( $this->exclude as $str ) {
+                $replacement = '<!--noptimize-->' . $str . '<!--/noptimize-->';
+                if ( false !== strpos( $this->content, $replacement ) ) {
+                    $this->content = str_replace( $replacement, $str, $this->content );
+                }
+            }
+
+            // Revslider data attribs somehow suffer from HTML optimization, this fixes that!
             if ( class_exists( 'RevSlider' ) && apply_filters( 'autoptimize_filter_html_dataattrib_cleanup', false ) ) {
-                $this->content = preg_replace( '#\n(data-.*$)\n#Um',' $1 ', $this->content );
+                $this->content = preg_replace( '#\n(data-.*$)\n#Um', ' $1 ', $this->content );
                 $this->content = preg_replace( '#<[^>]*(=\"[^"\'<>\s]*\")(\w)#', '$1 $2', $this->content );
             }
 
-            // restore noptimize
-            $this->content = $this->restore_noptimize($this->content);
             return true;
         }
 
-        //Didn't minify :(
+        // Didn't minify :(
         return false;
     }
 
-    //Does nothing
+    /**
+     * Doesn't do much in case of HTML (no cache in css/js sense there)
+     *
+     * @return true
+     */
     public function cache()
     {
-        //No cache for HTML
         return true;
     }
 
-    //Returns the content
+    /**
+     * Returns the HTML markup.
+     *
+     * @return string
+     */
     public function getcontent()
     {
         return $this->content;
